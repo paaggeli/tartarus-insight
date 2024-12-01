8fb3fcaff651b7309c73b9418483b5513a18e09c
panos <paaggeli@gmail.com>
2024-12-01 16:59:05 +0200

    Add functionality to close mobile menu when a navigation link is clicked & fixed the close menu icon position

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 const hamburger = document.querySelector('.hamburger');
 const closeMenu = document.querySelector('.close-mobile-menu');
 const navMenu = document.querySelector('#navMenu');
+const navLinks = document.querySelectorAll('#navMenu ul li a');
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.add('active');
@@ -10,7 +11,17 @@ hamburger.addEventListener('click', () => {
 });
 
 closeMenu.addEventListener('click', () => {
+    closeNavMenu();
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        closeNavMenu();
+    });
+});
+
+function closeNavMenu() {
     navMenu.classList.remove('active');
     hamburger.style.display = 'block';
     closeMenu.style.display = 'none';
-});
+}
